1db3bce185d5c35479df92f99334e221fbc5a81c
Jacob Hartmann <ohayo.jacob@gmail.com>
2021-04-30 12:41:30 +0200

    updated port thingy

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"net/http"
-	//"os"
+	"os"
 )
 
 func main() {
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./"))))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	http.ListenAndServe(":"+port, nil)
 	//if os.Args[1] == "local" {
-		http.ListenAndServe(":3000", nil)
+		//http.ListenAndServe(":3000", nil)
 	//}
 	//if os.Args[1] == "ext" {
 		//http.ListenAndServe(":35291", nil)
